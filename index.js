6d0172dc8a0117c08b869bcee48ef590cb6613b6
vanshika2723 <vanshikakhandelwal102@gmail.com>
2024-06-08 11:48:18 +0530

    basic js

---
+++ index.js
@@ -0,0 +1,122 @@
+// //var
+// var num = 10;
+// {
+//     num = 20;
+// }
+// console.log("var",num)
+
+// //let
+// let num1 = 10
+
+// {
+//     num1 = 20
+// }
+// console.log("let",num1)
+
+// //const
+// const num2 ={"name":"trisha"}
+
+// {
+//     num2.name="hii"
+// }
+// console.log("const",num2)
+
+
+// var num = 10
+
+// {
+//     var num = 20
+//     num = 30
+//     console.log(num)
+// }
+
+// console.log(num)
+
+// let num1 = 10
+// {
+//     let num1 = 20
+//     num1= 30
+//     console.log(num1)
+// }
+// console.log(num1)
+
+// const num2 = 10
+// {
+//     const num = 20
+//     console.log(num2)
+// }
+// console.log(num2);
+
+
+// let num1 =5;
+// let num2=10;
+
+// let sum =  num1 + num2
+// console.log(sum)
+
+// let sub = num1 - num2
+// console.log(sub)
+
+// let divide = num1/num2
+// console.log(divide)
+
+// let mode = num1%num2
+// console.log(mode)
+
+// let mul = num1*num2
+// console.log(mul)
+
+// let sum = --num1 + ++num2 + num1-- + num2++
+// console.log(sum)
+// console.log(num1)
+// console.log(num2)
+
+// let checkin =true
+// let checkout =true
+// console.log(checkin = checkout)
+
+// let checkin ="world"
+// let checkout ="hello"
+// console.log(checkin == checkout)
+
+// let checkin ="hello"
+// let checkout =""
+// // let obj={}
+// // // let arr;
+// // let arr=""
+// // console.log(typeof arr)
+// // console.log(typeof obj)
+// // console.log(typeof checkin)
+// // console.log(typeof checkout)
+// // console.log(checkin == checkout)
+// // console.log(checkin === checkout)
+// console.log(checkin)
+
+
+// let  checkin = 10
+// let checkout = 20
+// let result = (checkin  == checkout)?"yes":"no"
+// console.log(result);
+
+//ternary operator
+// let a = 10;
+// let b=20;
+// let c=30;
+// let result=(a>b && a>c)?"a":(b>c)?"b":"c"
+//  console.log(result);
+
+
+// let age=20
+// let result =(age>=18)?"you can vote":"not vote"
+// console.log(result);
+
+// let num1 = 20;
+// let num2 = 30;
+// let result = (num1 > num2)?"yes":"no";
+// let result1 = (num1 > num2)?num1:num2;
+// console.log(result);
+// console.log(result1);
+
+// let num1 = {"key":"val"}
+// let arr = [1,2,3]
+
